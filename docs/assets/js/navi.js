ff142e0e8533ba76891dc69c039bd0b7ae4d5863
Oleg <82386871+a374ru@users.noreply.github.com>
2022-08-11 00:16:45 +0300

    777

--- docs/assets/js/navi.js
+++ docs/assets/js/navi.js
@@ -42,7 +42,7 @@ if (localStorage.getItem("az") != 1) {
 function switchColorScheme() {
 
 
-	whiteBG = localStorage.getItem("color", '#fffbf6');
+	whiteBG = '#fffbf6';
 	darkBG = '#180000';
 	naviLight = `box-shadow: 0px 10 22 #fffaf5; background:linear-gradient(180deg, ${whiteBG} 0%, ##A78F5B 3%)`;
 	naviDark = `box-shadow: 0px 20px 52px #611816; background:linear-gradient(180deg, ${darkBG} 97%, red 1%)`;
