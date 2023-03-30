66dbe9bfb2deefde8dc5832988f60eaa37f0f47a
Oleg <82386871+a374ru@users.noreply.github.com>
2023-03-30 14:00:07 +0300

    -No commits yet.

--- docs/assets/js/navi.js
+++ docs/assets/js/navi.js
@@ -190,7 +190,7 @@ home</span></a></div>`;
 				element = element.slice(0, element.length - 3) + "…";
 			}
 
-			htmlString += `<div class="navi-item"><a title="${pageDict[key]}" href="/${folderProjectOfGitHub}${key}#navi">${element}</a> </div>`;
+			htmlString += `<div class="navi-item"><a title="${pageDict[key]}" href="/#navi${folderProjectOfGitHub}${key}#navi">${element}</a> </div>`;
 			counter += 1;
 
 		}
