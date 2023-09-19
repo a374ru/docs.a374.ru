b3d41f4c6bc2f3639cc423afc816995ff655b80f
Oleg <82386871+a374ru@users.noreply.github.com>
2023-09-19 18:21:45 +0300

    777 ••• 2023 YSTM-MAC

--- docs/assets/js/navi.js
+++ docs/assets/js/navi.js
@@ -29,7 +29,7 @@ pageDict = {
 	"search-result": "Поиск 🔍",
 	"nash-ncal": "NCAL",
 	"navi-page": "Оглавление",
-	"nash-nvm": "NMV",
+	"nash-nvm": "NVM",
 	// "expenses": "Затраты",
 };
 // ========== to local Storage ======================
